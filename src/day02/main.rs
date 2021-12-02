c69d7c5282fbe13cb62577380f8e5cc6e8bb602c
Mathieu Lemay <mathieu.lemay@broadsign.com>
2021-12-02 09:17:38 -0500

    Day 02

--- src/day02/main.rs
+++ src/day02/main.rs
@@ -3,8 +3,56 @@ use std::time::Instant;
 
 use aoc_2021::get_input;
 
+fn parse_commands(input: &[String]) -> Vec<(&str, u32)> {
+    input
+        .iter()
+        .map(|s| s.split_once(' ').unwrap())
+        .map(|(cmd, n)| (cmd, n.parse().unwrap()))
+        .collect()
+}
+
+fn part1(commands: &[(&str, u32)]) -> u32 {
+    let mut pos = 0;
+    let mut depth = 0;
+
+    for (cmd, n) in commands {
+        match *cmd {
+            "forward" => pos += n,
+            "down" => depth += n,
+            "up" => depth -= n,
+            _ => panic!("Invalid command: {}", cmd),
+        }
+    }
+
+    pos * depth
+}
+
+fn part2(commands: &[(&str, u32)]) -> u32 {
+    let mut pos = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    for (cmd, n) in commands {
+        match *cmd {
+            "forward" => {
+                pos += n;
+                depth += aim * n;
+            }
+            "down" => aim += n,
+            "up" => aim -= n,
+            _ => panic!("Invalid command: {}", cmd),
+        }
+    }
+
+    pos * depth
+}
+
 fn solve(input: &[String]) -> (impl Display, impl Display) {
-    (0, 0)
+    let commands = parse_commands(input);
+
+    let p1 = part1(&commands);
+    let p2 = part2(&commands);
+    (p1, p2)
 }
 
 fn main() {
@@ -19,4 +67,51 @@ fn main() {
     println!("Part 1: {}", r1);
     println!("Part 2: {}", r2);
     println!("Duration: {:.3}ms", t);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{parse_commands, part1, part2};
+
+    #[test]
+    fn test_p1() {
+        let input = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+
+        let input = input
+            .split("\n")
+            .map(|v| v.to_string())
+            .collect::<Vec<String>>();
+
+        let commands = parse_commands(&input);
+
+        let res = part1(&commands);
+
+        assert_eq!(150, res);
+    }
+
+    #[test]
+    fn test_p2() {
+        let input = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+
+        let input = input
+            .split("\n")
+            .map(|v| v.to_string())
+            .collect::<Vec<String>>();
+
+        let commands = parse_commands(&input);
+
+        let res = part2(&commands);
+
+        assert_eq!(900, res);
+    }
 }
