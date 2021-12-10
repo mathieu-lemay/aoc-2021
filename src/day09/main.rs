940d5a8e90227589c746ea20e2b4a088e042b55d
Mathieu Lemay <acidrain1@gmail.com>
2021-12-09 19:45:06 -0500

    wip d9

--- src/day09/main.rs
+++ src/day09/main.rs
@@ -97,7 +97,83 @@ fn part_2(values: &[Vec<u32>]) -> usize {
     let mut sizes = basins.iter().map(|b| b.len()).collect::<Vec<usize>>();
     sizes.sort_by(|a, b| b.cmp(a));
 
+    println!("{:?}", sizes.iter().take(3).collect::<Vec<&usize>>());
     sizes[0..3].iter().product::<usize>()
+}
+
+#[derive(Debug)]
+struct Chunk {
+    row: usize,
+    start: usize,
+    end: usize,
+}
+
+impl Chunk {
+    fn len(&self) -> usize {
+        self.end + 1 - self.start
+    }
+
+    fn overlaps(&self, other: &Chunk) -> bool {
+        self.start <= other.end && self.end >= other.start
+    }
+}
+
+fn get_chunks(row: &[u32], row_idx: usize) -> Vec<Chunk> {
+    let mut idx = 0;
+    let max = row.len();
+
+    let mut chunks = Vec::new();
+
+    loop {
+        while idx < max && row[idx] == 9 {
+            idx += 1;
+            continue;
+        }
+
+        if idx == max {
+            break;
+        }
+
+        let s = idx;
+        while idx < max && row[idx] != 9 {
+            idx += 1
+        }
+
+        let e = idx;
+
+        chunks.push(Chunk {
+            start: s,
+            end: e - 1,
+            row: row_idx,
+        })
+    }
+
+    chunks
+}
+
+fn part_2b(values: &[Vec<u32>]) -> usize {
+    let mut basins: Vec<Vec<Chunk>> = Vec::new();
+
+    for (idx, row) in values.iter().enumerate() {
+        for chunk in get_chunks(row, idx) {
+            match basins.iter_mut().find(|b| {
+                b.iter()
+                    .any(|c| idx > 0 && c.row == idx - 1 && chunk.overlaps(&c))
+            }) {
+                Some(b) => b.push(chunk),
+                None => basins.push(vec![chunk]),
+            };
+        }
+    }
+
+    let mut sizes = basins
+        .iter()
+        .map(|b| b.iter().map(|c| c.len()).sum())
+        .collect::<Vec<usize>>();
+    sizes.sort();
+
+    println!("{:?}", sizes.iter().rev().take(3).collect::<Vec<&usize>>());
+    sizes.iter().rev().take(3).product()
 }
 
 fn solve(input: &[String]) -> (impl Display, impl Display) {
@@ -112,8 +188,13 @@ fn solve(input: &[String]) -> (impl Display, impl Display) {
 
     let p1 = part_1(&values);
     let p2 = part_2(&values);
+    let p2b = part_2b(&values);
 
-    (p1, p2)
+    assert_eq!(468, p1);
+    assert_eq!(1280496, p2);
+    assert_eq!(1280496, p2b);
+
+    (p1, 0)
 }
 
 fn main() {
@@ -132,7 +213,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use crate::{part_1, part_2};
+    use crate::{part_1, part_2, Chunk};
 
     static TEST_INPUT: &str = "2199943210
 3987894921
@@ -166,5 +247,21 @@ mod tests {
             .collect::<Vec<Vec<u32>>>();
 
         assert_eq!(1134, part_2(&input));
+    }
+
+    #[test]
+    fn test_chunk_overlaps() {
+        let c1 = Chunk {
+            start: 2,
+            end: 4,
+            row: 0,
+        };
+        let c2 = Chunk {
+            start: 1,
+            end: 5,
+            row: 0,
+        };
+        assert!(c1.overlaps(&c2));
+        assert!(c2.overlaps(&c1));
     }
 }
